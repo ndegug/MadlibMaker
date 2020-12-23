4020cc7eef8668a096588f2b8c42803342f4e9c7
ndegug <69177716+ndegug@users.noreply.github.com>
2020-12-23 18:10:46 -0500

    Delete blank maker.py

--- blank maker.py
+++
@@ -1,8 +0,0 @@
-
-
-# todo: convert keywords to the format $\underset{Ajective}{\rule{2.5cm}{0.15mm}}$ Where the 25cm is the line length and .15mm is the line thickness
-import rule as rule
-
-type= "adjective"
-
-print('$\\underset{'+type+'}{\\rule{2.5cm}{0.15mm}}$')
