2f53bb3d9c92d1f2edaf145aec8ca7c486db6e0c
ndegug <deguglielmon@wit.edu>
2021-02-15 18:33:07 -0500

    Long_strings file added

--- MasterPythonMadlibMaker.py
+++ MasterPythonMadlibMaker.py
@@ -1,5 +1,5 @@
 from MadlibMakerHelpers import *
-
+from long_strings import *
 from pip._vendor.distlib.compat import raw_input
 
 outlist = []
@@ -11,10 +11,7 @@ if not os.path.isdir(os.path.join(os.getcwd(), "inputs")):
 if not os.path.isdir(os.path.join(os.getcwd(), "outputs")):
     os.mkdir(os.path.join(os.getcwd(), "outputs"))
 
-choice = raw_input(
-    "Welcome to the Madlib Maker\nHere you can compose and fill in madlibs using our specialized syntax.\nTo begin, "
-    "please select from the following menu options:\n1. Type madlib\n2. Use a madlib from the \"inputs\" folder.\n3. "
-    "Instructions\n")
+choice = raw_input(welcome)
 if choice == "1":
     # manual input
     cont = raw_input("Enter the madlib below:\n")
@@ -52,48 +49,11 @@ elif choice == "2":
     file_read()
     from MadlibMakerHelpers import inputList
     from MadlibMakerHelpers import custom
-    # filename = raw_input("Which file would you like to process? (type the name with no extension)\n")
-    # # saving name of custom word file
-    # customfile = filename + "_cts.txt"
-    # # reading main content file
-    # choice = os.path.join('inputs', filename + ".txt")
-    # my_file = open(choice, "r")
-    # cont = my_file.read()
-    # inputList = cont.split(" ")
-    # if os.path.exists(os.path.join('inputs', customfile)):
-    #     choice = os.path.join('inputs', customfile)
-    #     with open(choice) as f:
-    #         data = f.read()
-    #     custom = json.loads(data.replace("\'", "\""))
-    #     f.close()
-    # elif not os.path.exists(os.path.join('inputs', customfile)) and re.search(customreg, str(inputList)) is not None:
-    #     cust_config()
-    #     file_write(str(custom), filename, 'inputs', '_cts.txt')
-    # else:
-    #     pass
 elif choice == "3":
     while choice != "q":
-        choice = raw_input("Which would you like to learn about:\n1. How to write Madlibs\n2. "
-                           "Syntax\n3. Custom Words\n4. Saving and printing madlibs\nType \"q\" to quit\n")
+        choice = raw_input(instruct0)
         if choice == "1":  # todo add a "process pre-made file" instructions
-            print("Here in the Madlib Maker, you can write a Madlib directly or process a file you've already "
-                  "typed.\nFor "
-                  "each blank, you must type a keyword specific to the word category you desire.\nThese keywords "
-                  "typically follow "
-                  "the syntax of 3 letters preceded by a \"/\", for example,\ntyping the keyword \"/adj\" will prompt "
-                  "the "
-                  "program to ask the user for an \"Adjective.\"\nSee the Syntax section for the full list of "
-                  "keywords.\nYou can add a number at the end of any keyword in the program's dictionary to repeat it "
-                  "within\nthe "
-                  "same madlib.\n\nFor example, if the input is:\n\"We have a pet /ani1, he's a good /ani1!\"\nand "
-                  "you "
-                  "enter \"dog\" when prompted, the program will fill in both at the same time resulting in:\n\"We "
-                  "have "
-                  "a pet dog, he's a good dog!\"\n\nIf you wish to use a word that is not in "
-                  "this program's selection,\nyou may enter it as a "
-                  "custom word. These custom words can be\nconfigured before writing the Madlib.\nSee the \"Custom "
-                  "Words\" "
-                  "section for further details.\n")
+            print(instruct1)
         elif choice == "2":
             print("The default keys and word categories are as follows:")
             i = 0
@@ -107,33 +67,9 @@ elif choice == "3":
             print("\n")
 
         elif choice == "3":
-            print(
-                "Custom words allow you to add your own word categories if they are not already stored in this "
-                "program's dictionary.\nIf you wanted the program to call out something obscure like \"Baseball "
-                "player\" "
-                "or \"High school friend\", you can use this feature to do so.\nWhen questioned whether you wish to "
-                "configure the custom words, type \"yes\""
-                "and enter these words sequentially. Once your custom words are configured,\nuse the keyword "
-                "sequence: "
-                "\"/ct1\" where the number indicates which sequential word you want to be called there.\n\nIf you "
-                "want a specific custom word entry to automatically entered several times, use a numbered\ncustom "
-                "word key such as:\"/ct1_1.\" This key will be interperated as a numbered word (see the \"How\nto "
-                "write Maldibs\" instructions section "
-                "for more details) and any word that is used to replace it will then replace all other\nwords "
-                "with that same key")
+            print(instruct3)
         elif choice == "4":
-            print("After writing a madlib, you can save it to the \"inputs\" folder for later use.\nThis is usefull "
-                  "if you have a lengthy madlib that you wish to fill in later or multiple times.\nIf you wish to "
-                  "type a madlib outside of this program, note that the current build accepts text files only and "
-                  "refrain from adding a title/heading "
-                  "as it will be entered later in the program.\nReview the \"Syntax\" section of the instructions for "
-                  "the proper syntax of an input file.\n\nWith a madlib typed or selected, you may chose to fill it "
-                  "within the program or\nprint it as a traditional unfilled maldib on paper.\nOutputs of either "
-                  "nature will be saved to the \"outputs\" folder.\nThe filled madlibs will be saved as a text file "
-                  "while traditional madlibs will be converted into\nHTML files designed to display the heading and "
-                  "blanks with word categories underneath. Drag the\nHTML file into any web browser to view it in "
-                  "the traditional madlib format, then use the respective\nbrowser's print feature to print a "
-                  "physical copy of the madlib.")
+            print(instruct4)
         elif choice == "q":
             print("I hope these instructions helped, start the program again to give it a try!\nHave a good day!")
             exit()
@@ -204,7 +140,7 @@ for word in inputList:
         regkey = re.findall(numcustreg, word)
         realkey = ''.join(regkey)
 
-        base = re.findall(customreg, word)
+        base = re.findall(customreg, word)#todo make latword/html function that will let us delete this
         base = ''.join(base)
 
         tailkey = re.findall(tail, word)
