2377bd5afc70e205aa2d5223d1067ee61aafc7db
NDegug <deguglielmon@wit.edu>
2025-05-11 22:27:14 -0400

    Redirected loads to "file_choice" and imported all generic words. Now having issues with large madlib load

--- Cusotm_refactor.py
+++ Cusotm_refactor.py
@@ -9,7 +9,16 @@ from docx import Document
 
 generic_words = { # reminder: do not add any keywords that are the same as ignored words
                  '/adj': 'Adjective', '/nou': 'Noun', '/pln': 'Plural noun',
-                 '/ver': 'Verb'}
+                 '/ver': 'Verb', '/vng': 'Verb ending in \"ing\"', '/ved': 'Past tense verb',
+                 '/ves': 'Verb ending in \"s\"', '/adv': 'adverb',
+                 '/num': 'Number', '/nam': 'Name', '/cel': 'Celebrity',
+                 '/per': 'Person', '/pir': 'Person in room', '/thi': 'Thing',
+                 '/pla': 'Place', '/job': 'Job', '/ran': 'Random Word',
+                 '/rex': 'Random Exclamation', '/tvs': 'TV Show', '/mov': 'Movie',
+                 '/mtv': "Movie/TV show", '/ins': 'Insulting name', '/phr': 'Random Phrase',
+                 '/fam': 'Family member (title)', '/foo': 'Food', '/ani': 'Animal',
+                 '/fic': 'Fictional Character', '/act': 'Activity', '/bod': 'Body Part', '/flu': 'Fluid',
+                 '/emo': 'Emotion', '/noi': 'noise', '/eve': 'Event', '/fos': 'Plural food', '/fur': 'Furniture'}
 ignored_words = ['/her', '/she', '/She']  # words that resemble generic words that will be ignored
 numword_dic = {}
 unnumbered = "(/...)"
@@ -256,10 +265,10 @@ class MadlibApp:
         if self.custom_keys and self.custom=={}:
 
             self.custom_configure_window() #configure customs, file_choice will be executted as well
-        elif self.mode==0: #if manual input and no others are needed
+        else: #if manual input and no others are needed
             self.file_choice()
-        else:
-            self.advance_to_second()
+        #else:
+        #    self.advance_to_second()
 
 
     def custom_configure_window(self):
